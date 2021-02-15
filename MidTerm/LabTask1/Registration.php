c9eeeffe05a91a8f2d99e5b8ac0e6fe37e8cf001
skragib <77957493+skragib@users.noreply.github.com>
2021-02-15 11:21:27 +0600

    Create MidTerm

---
+++ MidTerm/LabTask1/Registration.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Labtask 1</title>
+
+</head>
+<body>
+<h1> Registration </h1>
+
+<form>
+<table>
+<tr>
+<td>
+First Name: </td><td><input type="text" id="f" name="fname"></td>
+</tr>
+
+<tr><td>Email:</td><td> <input type="text" id="i" name="iname"></td>
+</tr>
+<tr>
+<td>
+User Name: </td><td><input type="text" id="g" name="gname"></td>
+</tr>
+<tr>
+<td>Password:</td><td> <input type="password" id="j" name="jname"><td>
+</tr>
+<tr>
+<td>Confirm Password:</td><td> <input type="password" id="k" name="kname"><td>
+</tr>
+</table>
+
+
+<p>Gender:</p>
+<input type="radio" id="male" name="gender" value="male">
+Male
+<input type="radio" id="female" name="gender" value="female">
+Female
+<input type="radio" id="other" name="gender" value="other">
+Other
+<br>
+<p>Date of Birth:</p>
+<input type="date" id="birthday" name="birthday">
+<br><br>
+
+
+
+<table><tr><td>
+<input type ="submit" value="Submit"></td>
+
+<td>
+<input type ="reset" value="Reset"></td>
+</tr>
+</table>
+
+</form>
+</body>
+</html>
